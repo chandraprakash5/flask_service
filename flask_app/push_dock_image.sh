7f70d6a52171e130d784cfcc2ac8ab521393440f
himanshu <himanshu119.iitkgp@gmail.com>
2019-01-30 23:16:20 +0530

    changing push_docker_image location

--- flask_app/push_dock_image.sh
+++ flask_app/push_dock_image.sh
@@ -3,7 +3,9 @@ set -e
 
 IMAGE_TAG=$(git log --format="%H" -n 1)
 echo $pwd
+
 cd flask_app
 docker build -t flask-sample-one:$IMAGE_TAG .
 docker tag flask-sample-one:$IMAGE_TAG $DOCKER_ID_USER/flask-sample-one:$IMAGE_TAG
 docker push $DOCKER_ID_USER/flask-sample-one
+cd ..
