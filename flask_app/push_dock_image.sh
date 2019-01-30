2ca5dd96ca2c4a99a680a08bdd846abfe7a1f8b7
himanshu <himanshu119.iitkgp@gmail.com>
2019-01-30 23:21:00 +0530

    added docker login

--- flask_app/push_dock_image.sh
+++ flask_app/push_dock_image.sh
@@ -5,6 +5,7 @@ IMAGE_TAG=$(git log --format="%H" -n 1)
 echo $pwd
 
 cd flask_app
+docker login
 docker build -t flask-sample-one:$IMAGE_TAG .
 docker tag flask-sample-one:$IMAGE_TAG $DOCKER_ID_USER/flask-sample-one:$IMAGE_TAG
 docker push $DOCKER_ID_USER/flask-sample-one
