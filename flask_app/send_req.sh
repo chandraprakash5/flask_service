3fce1009a700c3d108ae4f09c81e13272328cf74
Chandra Prakash <chandraprakash54@hotmail.com>
2019-01-31 00:08:45 +0530

    quotes

--- flask_app/send_req.sh
+++ flask_app/send_req.sh
@@ -13,8 +13,10 @@ last_commit_subject=$(git log -1 --pretty=format:'%s' | gzip | base64)
 last_commit_branch=$(git rev-parse --abbrev-ref HEAD)
 commit_info='{"author":"'$last_commit_author'","email":"'$last_commit_email'","timestamp":"'$last_commit_epoch_time_sec'","subject_gz_b64":"'$last_commit_subject'","branch":"'$last_commit_branch'"}'
 commit_info=$(echo $commit_info | gzip | base64)
-values='{"value_gz_b64":"'"$value"'","commit_id":"'"$commit_id"'","application_id":"'"$application_id"'","image_tag":"'"$image_tag"'","commit_info_gz_b64":"'"$commit_info"'"}'
+#values='{"value_gz_b64":"'"$value"'","commit_id":"'"$commit_id"'","application_id":"'"$application_id"'","image_tag":"'"$image_tag"'","commit_info_gz_b64":"'"$commit_info"'"}'
+values={"value_gz_b64":"$value","commit_id":"$commit_id","application_id":"$application_id","image_tag":"$image_tag","commit_info_gz_b64":"$commit_info"}
 echo "##########################################"
 echo $values
 echo "##########################################"
 curl -v -i http://13.233.82.112:3000/v1/update -H "Authorization: DECL_API_KEY apikey=$DECLAYER_API_KEY" -H 'content-type: application/json' --data "$values"
+#curl -v -i http://13.233.82.112:3000/v1/update -H "Authorization: DECL_API_KEY apikey=$DECLAYER_API_KEY" -H 'content-type: application/json' --data "'"$values"'"
