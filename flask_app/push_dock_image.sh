15fb0a3b800a2b3ddaa77c921de72022d9ba2a01
Chandra Prakash <chandraprakash54@hotmail.com>
2019-01-30 23:42:23 +0530

    Testing

--- flask_app/push_dock_image.sh
+++ flask_app/push_dock_image.sh
@@ -9,7 +9,7 @@ echo $PWD
 echo `whoami`
 
 cd flask_app
-
+docker version
 docker build -t flask-sample-one:$IMAGE_TAG .
 docker tag flask-sample-one:$IMAGE_TAG $DOCKER_ID_USER/flask-sample-one:$IMAGE_TAG
 #docker push $DOCKER_ID_USER/flask-sample-one
