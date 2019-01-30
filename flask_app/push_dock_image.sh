fcd4a8436ca341d8acdd5cfa3a2d794879513b75
himanshu <himanshu119.iitkgp@gmail.com>
2019-01-30 23:22:59 +0530

    added echoes in push docker image script

--- flask_app/push_dock_image.sh
+++ flask_app/push_dock_image.sh
@@ -2,10 +2,12 @@
 set -e
 
 IMAGE_TAG=$(git log --format="%H" -n 1)
-echo $pwd
+echo $IMAGE_TAG
+echo $DOCKER_ID_USER
+echo $PWD
 
 cd flask_app
-docker login
+
 docker build -t flask-sample-one:$IMAGE_TAG .
 docker tag flask-sample-one:$IMAGE_TAG $DOCKER_ID_USER/flask-sample-one:$IMAGE_TAG
 docker push $DOCKER_ID_USER/flask-sample-one
