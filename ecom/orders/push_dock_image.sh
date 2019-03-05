6f6893a7a626525e3fd91fdd472af07b3cd89d12
Chandra Prakash <chandraprakash54@hotmail.com>
2019-03-05 21:22:21 +0530

    Changing image repo

--- ecom/orders/push_dock_image.sh
+++ ecom/orders/push_dock_image.sh
@@ -2,10 +2,10 @@
 set -e
 
 export PATH=$PATH:/home/jenkins/.local/bin
-DOCKER_ID_USER=chanprak
 IMAGE_TAG=$(git log --format="%H" -n 1)
 
 cd ecom/orders
+DOCKER_ID_USER=chanprak
 docker build -t ecom-orders:$IMAGE_TAG  .
 docker tag ecom-orders:$IMAGE_TAG $DOCKER_ID_USER/ecom-orders:$IMAGE_TAG
 docker push $DOCKER_ID_USER/ecom-orders:$IMAGE_TAG
--- ecom/reviews/push_dock_image.sh
+++ ecom/reviews/push_dock_image.sh
@@ -5,8 +5,8 @@ export PATH=$PATH:/home/jenkins/.local/bin
 IMAGE_TAG=$(git log --format="%H" -n 1)
 
 cd ecom/reviews
-$(aws ecr get-login --no-include-email --region us-west-2)
-docker build -t ecom/reviews:$IMAGE_TAG  .
-docker tag ecom/reviews:$IMAGE_TAG 725566882860.dkr.ecr.us-west-2.amazonaws.com/ecom/reviews:$IMAGE_TAG
-docker push 725566882860.dkr.ecr.us-west-2.amazonaws.com/ecom/reviews:$IMAGE_TAG
+DOCKER_ID_USER=chanprak
+docker build -t ecom-reviews:$IMAGE_TAG  .
+docker tag ecom-reviews:$IMAGE_TAG $DOCKER_ID_USER/ecom-reviews:$IMAGE_TAG
+docker push $DOCKER_ID_USER/ecom-reviews:$IMAGE_TAG
 cd ../..
