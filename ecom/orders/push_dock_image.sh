49060d203945acb565678b4b7b8ec30d9ae2706c
Chandra Prakash <chandraprakash54@hotmail.com>
2019-03-05 20:18:52 +0530

    Changing image repository

--- ecom/orders/push_dock_image.sh
+++ ecom/orders/push_dock_image.sh
@@ -2,11 +2,12 @@
 set -e
 
 export PATH=$PATH:/home/jenkins/.local/bin
+DOCKER_ID_USER=chanprak
 IMAGE_TAG=$(git log --format="%H" -n 1)
 
 cd ecom/orders
-$(aws ecr get-login --no-include-email --region us-west-2)
-docker build -t ecom/orders:$IMAGE_TAG  .
-docker tag ecom/orders:$IMAGE_TAG 725566882860.dkr.ecr.us-west-2.amazonaws.com/ecom/orders:$IMAGE_TAG
-docker push 725566882860.dkr.ecr.us-west-2.amazonaws.com/ecom/orders:$IMAGE_TAG
+docker build -t ecom-orders:$IMAGE_TAG  .
+docker tag ecom-orders:$IMAGE_TAG $DOCKER_ID_USER/ecom-orders:$IMAGE_TAG
+docker push $DOCKER_ID_USER/ecom-orders:$IMAGE_TAG
 cd ../..
+
